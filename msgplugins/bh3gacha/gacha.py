1404c93468e56907262fbef70b6f7d96ebf06a94
linyuchen <lin.yu.chen@foxmail.com>
2020-07-24 08:40:06 +0800

    卡池parser

--- msgplugins/bh3gacha/gacha.py
+++ msgplugins/bh3gacha/gacha.py
@@ -5,7 +5,6 @@ import random
 from typing import List
 from dataclasses import dataclass
 
-
 PATH_CURRENT = os.path.dirname(__file__)
 PATH_DATA_DIR = os.path.join(PATH_CURRENT, "概率")
 PATH_扩充装备概率 = os.path.join(PATH_DATA_DIR, "扩充装备.txt")
@@ -19,40 +18,47 @@ class Equipment:
     name: str
 
 
-equipments = []
+class TextParser:
+    def __init__(self, path: str):
+        with open(path, "rb") as f:
+            for line in f.readlines():
+                line = line.decode("utf8")
+                line = line.strip()
+                if not line or line.startswith("#"):
+                    continue
+                elif line.startswith("!"):
+                    self.parse_category(line)
+                else:
+                    self.parse_item(line)
+
+    def parse_category(self, line: str):
+        raise NotImplementedError
+
+    def parse_item(self, item: str):
+        raise NotImplementedError
 
 
-def read():
-    with open(PATH_扩充装备概率, "rb") as f:
-        current_star = 0
-        current_type = None
-        current_probability = 0
+class EquipmentProbabilityParser(TextParser):
 
-        for line in f.readlines():
-            line = line.decode("utf8")
-            line = line.strip()
-            if not line:
-                continue
-            if line.startswith("#"):
-                continue
-            elif line.startswith("!"):
-                line = line[1: -1]  # 去掉头部!号和尾部%号
-                line_tuple = line.split(" ")
-                current_star = int(line_tuple[0][0])  # 星级
-                current_type = line_tuple[1]  # 类型
-                current_probability = float(line_tuple[2])  # 概率
-            else:
-                _e = Equipment(probability=current_probability, type_name=current_type, star=current_star, name=line)
-                equipments.append(_e)
+    def __init__(self, path: str):
+        self.current_star = 0
+        self.current_type = None
+        self.current_probability = 0
+        self.equipments = []
+        super(EquipmentProbabilityParser, self).__init__(path)
+
+    def parse_category(self, line: str):
+        line = line[1: -1]  # 去掉头部!号和尾部%号
+        line_tuple = line.split()
+        self.current_star = int(line_tuple[0][0])  # 星级
+        self.current_type = line_tuple[1]  # 类型
+        self.current_probability = float(line_tuple[2])  # 概率
+
+    def parse_item(self, item: str):
+        _e = Equipment(probability=self.current_probability, type_name=self.current_type, star=self.current_star,
+                       name=item)
+        self.equipments.append(_e)
 
 
-read()
-pool = []
-for e in equipments:
-    e: Equipment
-    pool.extend([e] * int(e.probability * 1000))
-
-
-result = random.choice(pool)
-print(result)
-
+p = EquipmentProbabilityParser(PATH_扩充装备概率)
+print(p.equipments)
