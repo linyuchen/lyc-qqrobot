6569ebfa138fdd31f22945ddb2e3abda4eee2542
linyuchen <lin.yu.chen@foxmail.com>
2023-05-30 02:02:44 +0800

    chatgpt插件

--- msgplugins/chatgpt/__init__.py
+++ msgplugins/chatgpt/__init__.py
@@ -1,5 +1,5 @@
 from qqsdk.message import MsgHandler, GroupMsg, FriendMsg, BaseMsg
-from chatgpt import gpt_35
+from .chatgpt import gpt_35
 from ..cmdaz import CMD
 
 
@@ -7,7 +7,7 @@ class ChatGPT(MsgHandler):
     bind_msg_types = (GroupMsg, FriendMsg)
 
     def handle(self, msg: BaseMsg):
-        cmd = CMD("c")
+        cmd = CMD("#", param_len=1)
         if cmd.az(msg.msg):
             res = gpt_35(cmd.get_original_param())
             msg.reply(res)
