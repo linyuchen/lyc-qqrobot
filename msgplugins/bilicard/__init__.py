a6c60f372ccef3e20f32a624b6147bbd3b53c9bd
linyuchen <lin.yu.chen@foxmail.com>
2023-06-23 02:05:14 +0800

    B站视频卡片插件采用发送图片

--- msgplugins/bilicard/__init__.py
+++ msgplugins/bilicard/__init__.py
@@ -3,7 +3,7 @@ import os
 from qqsdk.message import MsgHandler, GroupMsg, FriendMsg
 from qqsdk.message.segment import MessageSegment
 from ..cmdaz import CMD
-from .bilicard import get_bv_id, gen_text
+from .bilicard import get_bv_id, gen_text, gen_image
 
 
 class BiliCardPlugin(MsgHandler):
@@ -12,11 +12,11 @@ class BiliCardPlugin(MsgHandler):
     def handle(self, msg: GroupMsg | FriendMsg):
         bvid = get_bv_id(msg.msg)
         if bvid:
-            text = gen_text(bvid)
-            if text:
-                msg.reply(text)
-
-            # if img_path:
-            #     reply_msg = MessageSegment.image_path(img_path)
-            #     msg.reply(reply_msg)
-            #     os.remove(img_path)
+            # text = gen_text(bvid)
+            # if text:
+            #     msg.reply(text)
+            img_path = gen_image(bvid)
+            if img_path:
+                reply_msg = MessageSegment.image_path(img_path)
+                msg.reply(reply_msg)
+                os.remove(img_path)
