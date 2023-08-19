88f2c739f07f9df6a3c357debac93d090e6130c1
linyuchen <lin.yu.chen@foxmail.com>
2023-08-20 00:35:34 +0800

    fix: sd send image

--- msgplugins/stable_diffusion/__init__.py
+++ msgplugins/stable_diffusion/__init__.py
@@ -1,9 +1,8 @@
 from functools import reduce
-from pathlib import Path
 
 import ifnude
 
-from qqsdk.message import MsgHandler, GroupMsg, FriendMsg
+from qqsdk.message import GroupMsg, FriendMsg
 from qqsdk.message.segment import MessageSegment
 from .sd import SDDraw
 from ..cmdaz import on_command
@@ -21,18 +20,20 @@ sd = SDDraw()
 
 @on_command("画图", alias=("sd", "画画", "绘图", "画一", "画个", "给我画", "帮我画", "画张"), param_len=1)
 def sd_draw(msg: GroupMsg | FriendMsg, args: list[str]):
-    img_paths = sd.txt2img(args[0])
+    # msg.reply("正在努力画画中（吭哧吭哧~），请稍等...")
+    try:
+        img_paths = sd.txt2img(args[0], width=512, height=512)
+    except Exception as e:
+        return
     for img_path in img_paths[:]:
         if ifnude.detect(str(img_path)):
             img_path.unlink()
             img_paths.remove(img_path)
     if img_paths:
-        reply_msg = reduce(lambda x, y: MessageSegment.image_path(x) + MessageSegment.image_path(y),
-                           img_paths,
-                           MessageSegment.image_path(img_paths[0]))
+        msgs = [MessageSegment.image_path(img_path) for img_path in img_paths]
+        reply_msg = reduce(lambda a, b: a + b, msgs)
         msg.reply(reply_msg)
         for i in img_paths:
             i.unlink(missing_ok=True)
     else:
         msg.reply("图片违规，已被删除")
-
