44d523312f8c158443f18844f5adf3f634d1685c
linyuchen <lin.yu.chen@foxmail.com>
2023-08-22 19:51:49 +0800

    opt: 错误日志加入错误报错的位置

--- common/logger/logger.py
+++ common/logger/logger.py
@@ -11,16 +11,16 @@ error_handler = logging.FileHandler(base_dir / f'error-{datetime.datetime.now().
 error_handler.setLevel(logging.ERROR)
 
 # 创建一个用于写入所有级别日志的handler
-all_handler = logging.FileHandler(base_dir / 'all.log')
-all_handler.setLevel(logging.DEBUG)
+info_handler = logging.FileHandler(base_dir / f'{datetime.datetime.now().date()}.log', encoding='utf-8')
+info_handler.setLevel(logging.INFO)
 
 # 定义日志格式
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(pathname)s[line:%(lineno)d]: %(message)s ')
 
 # 将格式应用到handler
 error_handler.setFormatter(formatter)
-all_handler.setFormatter(formatter)
+info_handler.setFormatter(formatter)
 
 # 将handler添加到logger对象中
 logger.addHandler(error_handler)
-logger.addHandler(all_handler)
+logger.addHandler(info_handler)
