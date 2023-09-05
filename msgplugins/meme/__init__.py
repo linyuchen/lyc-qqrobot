064dd0ea7ac0b9630d6bb6fa1e8eab416ad79993
linyuchen <lin.yu.chen@foxmail.com>
2023-09-05 13:06:19 +0800

    :sparkles: feat: 更多的戳一戳表情

--- msgplugins/meme/__init__.py
+++ msgplugins/meme/__init__.py
@@ -1,9 +1,8 @@
 import asyncio
 import random
 import tempfile
-from functools import reduce
 from pathlib import Path
-from typing import Callable, Dict, Any
+from typing import Callable
 
 from filetype import filetype
 from meme_generator.cli import get_meme
@@ -13,7 +12,7 @@ from qqsdk.message import GroupNudgeMsg, MessageSegment
 
 
 def create_meme_func(key: str, texts: list[str] = None,
-                     args: dict[str, str] | Callable[[None], dict] = None,
+                     args: dict[str, any] | Callable[[None], dict] = None,
                      images_len=1):
     if not texts:
         texts = []
@@ -27,6 +26,7 @@ def create_meme_func(key: str, texts: list[str] = None,
             new_text = text.replace("{from_name}", msg.from_member.get_name())
             new_text = new_text.replace("{target_name}", msg.target_member.get_name())
             texts[index] = new_text
+
         _meme = get_meme(key)
         loop = asyncio.new_event_loop()
         images = []
@@ -77,6 +77,12 @@ nudge_memes = (
     create_meme_func("funny_mirror"),  # 哈哈镜
     create_meme_func("garbage"),  # 垃圾桶
     create_meme_func("genshin_start"),  # 原神启动
+    create_meme_func("hammer"),  # 锤
+    create_meme_func("hit_screen"),  # 打穿屏幕
+    create_meme_func("hold_tight"),  # 抱紧
+    create_meme_func("hug_leg"),  # 抱大腿
+    create_meme_func("hutao_bite"),  # 胡桃啃
+    create_meme_func("incivilization"),  # 你刚才说的话不礼貌
     create_meme_func("fencing", images_len=2),  # 两熊猫打架
     create_meme_func("crawl", args=lambda: {"number": random.randint(1, 92)}),  # 爬
     create_meme_func("alike", args={"model": "loop"}),  # 永远喜欢
@@ -88,12 +94,107 @@ nudge_memes = (
     create_meme_func("douyin", texts=["{target_name}"], images_len=0),  # 让xx告诉你吧
     create_meme_func("fanatic", texts=["{target_name}"], images_len=0),  # 狂热粉
     create_meme_func("follow", texts=["{target_name}"]),  # 关注了你
-    create_meme_func("chanshenzi", texts=["你那叫喜欢吗？", "你那是馋她身子", "{target_name}下贱！"], images_len=0),  # 馋身子
+    create_meme_func("chanshenzi", texts=["你那叫喜欢吗？", "你那是馋她身子", "{target_name}下贱！"], images_len=0),
+    # 馋身子
+    create_meme_func("interview", images_len=2),  # 你刚才说的话不礼貌
+    create_meme_func("jiujiu"),
+    create_meme_func("karyl_point"),
+    create_meme_func("kick_ball"),
+    create_meme_func("kirby_hammer"),
+    create_meme_func("kiss", images_len=2),
+    create_meme_func("klee_eat", images_len=1),
+    create_meme_func("knock", images_len=1),
+    create_meme_func("listen_music", images_len=1),
+    create_meme_func("little_angel", images_len=1),
+    create_meme_func("loading", images_len=1),
+    create_meme_func("look_flat", images_len=1),
+    create_meme_func("look_this_icon", images_len=1),
+    create_meme_func("love_you", images_len=1),
+    create_meme_func("luoyonghao_say", images_len=0, texts=["{target_name}"]),
+    create_meme_func("luxun_say", images_len=0, texts=["{target_name}"]),
+    create_meme_func("maikease", images_len=0,
+                     texts=["美国前五星上将麦克阿瑟", "曾这样评价道", "如果让我去阻止{target_name}",
+                            "那么我宁愿去阻止上帝"]),
+    create_meme_func("mihoyo", images_len=1),
+    create_meme_func("mourning", images_len=1, args={"black": True}),
+    create_meme_func("my_friend", images_len=1, texts=["来点涩图"]),
+    create_meme_func("my_wife", images_len=1),
+    create_meme_func("need", images_len=1),
+    create_meme_func("nekoha_holdsign", images_len=0, texts=["{target_name}"]),
+    create_meme_func("nijika_holdsign", images_len=0, texts=["{target_name}"]),
+    create_meme_func("no_response", images_len=1),
+    create_meme_func("nokia", images_len=0, texts=["{target_name}"]),
+    create_meme_func("not_call_me", images_len=0, texts=["{target_name}"]),
+    create_meme_func("note_for_leave", images_len=1),
+    create_meme_func("oshi_no_ko", images_len=1),
+    create_meme_func("overtime", images_len=1),
+    create_meme_func("paint", images_len=1),
+    create_meme_func("painter", images_len=1),
+    create_meme_func("pass_the_buck", images_len=1, texts=["你的！"]),
+    create_meme_func("pat", images_len=1),
+    create_meme_func("perfect", images_len=1),
+    create_meme_func("petpet", images_len=1, args={"circle": True}),
+    create_meme_func("play", images_len=1),
+    create_meme_func("play_game", images_len=1),
+    create_meme_func("police", images_len=1),
+    create_meme_func("police1", images_len=1),
+    create_meme_func("pound", images_len=1),
+    create_meme_func("printing", images_len=1),
+    create_meme_func("prpr", images_len=1),
+    create_meme_func("punch", images_len=1),
+    create_meme_func("raise_image", images_len=1),
+    create_meme_func("raise_image", images_len=1),
+    create_meme_func("raise_sign", images_len=0, texts=["{target_name}带带我"]),
+    create_meme_func("read_book", images_len=1),
+    create_meme_func("repeat", images_len=2, texts=["来点涩图"]),
+    create_meme_func("rip", images_len=2),
+    create_meme_func("rip_angrily", images_len=1),
+    create_meme_func("rise_dead", images_len=1),
+    create_meme_func("roll", images_len=1),
+    create_meme_func("rub", images_len=2),
+    create_meme_func("safe_sense", images_len=1),
+    create_meme_func("scratch_head", images_len=1),
+    create_meme_func("scroll", images_len=0, texts=["{target_name}"]),
+    create_meme_func("shock", images_len=1),
+    create_meme_func("sit_still", images_len=1),
+    create_meme_func("smash", images_len=1),
+    create_meme_func("step_on", images_len=1),
+    create_meme_func("suck", images_len=1),
+    create_meme_func("support", images_len=1),
+    create_meme_func("symmetric", images_len=1, args={"direction": "left"}),
+    create_meme_func("symmetric", images_len=1, args={"direction": "right"}),
+    create_meme_func("symmetric", images_len=1, args={"direction": "top"}),
+    create_meme_func("symmetric", images_len=1, args={"direction": "bottom"}),
+    create_meme_func("tankuku_raisesign", images_len=1),
+    create_meme_func("taunt", images_len=1),
+    create_meme_func("teach", images_len=1),
+    create_meme_func("tease", images_len=1),
+    create_meme_func("think_what", images_len=1),
+    create_meme_func("throw", images_len=1),
+    create_meme_func("throw_gif", images_len=1),
+    create_meme_func("thump", images_len=1),
+    create_meme_func("thump_wildly", images_len=1),
+    create_meme_func("tightly", images_len=1),
+    create_meme_func("together", images_len=1),
+    create_meme_func("trance", images_len=1),
+    create_meme_func("turn", images_len=1),
+    create_meme_func("twist", images_len=1),
+    # create_meme_func("vibrate", images_len=1),
+    create_meme_func("wallpaper", images_len=1),
+    create_meme_func("walnut_pad", images_len=1),
+    create_meme_func("walnut_zoom", images_len=1),
+    create_meme_func("wave", images_len=1),
+    create_meme_func("what_I_want_to_do", images_len=1),
+    create_meme_func("what_he_wants", images_len=1),
+    create_meme_func("why_at_me", images_len=1),
+    create_meme_func("windmill_turn", images_len=1),
+    create_meme_func("wooden_fish", images_len=1),
+    create_meme_func("worship", images_len=1),
 )
 
 
 @on_command("",
-            bind_msg_type=(GroupNudgeMsg, ),
+            bind_msg_type=(GroupNudgeMsg,),
             auto_destroy=False,
             is_async=True,
             cmd_group_name="戳一戳表情")
