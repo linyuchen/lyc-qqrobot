0b116cb5ea467195f95734a9eb57b3c13e7a3060
linyuchen <lin.yu.chen@foxmail.com>
2023-09-10 20:35:57 +0800

    fix: 来点涩图改成小图

--- msgplugins/lolicon_img.py
+++ msgplugins/lolicon_img.py
@@ -4,15 +4,15 @@ from msgplugins.msgcmd import on_command
 from qqsdk.message import GeneralMsg, MessageSegment
 
 
-@on_command("冲", alias=("冲冲冲", "来点色图", "来点涩图"),
+@on_command("冲", alias=("色图", "涩图", "冲冲冲", "来点色图", "来点涩图"),
             param_len=-1, is_async=True, cmd_group_name="来点色图")
 def lolicon_img(msg: GeneralMsg, msg_params: list[str]):
     try:
-        api_url = "https://api.lolicon.app/setu/v2?size=regular"
+        api_url = "https://api.lolicon.app/setu/v2?size=small"
         if msg_params:
             api_url += "&tag=" + "&tag=".join(msg_params)
         data = requests.get(api_url).json()["data"]
-        img_url = data[0]["urls"]["regular"]
+        img_url = data[0]["urls"]["small"]
     except Exception as e:
         return msg.reply(f"图片获取失败 {e}")
     msg.reply(MessageSegment.image(img_url))
