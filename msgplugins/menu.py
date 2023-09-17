f521359424a4e2f9cddf6e28388829befec07fac
linyuchen <lin.yu.chen@foxmail.com>
2023-09-17 14:46:10 +0800

    opt: 菜单优化

--- msgplugins/menu.py
+++ msgplugins/menu.py
@@ -1,6 +1,5 @@
-import config
-from qqsdk.message import MsgHandler, GroupMsg, FriendMsg
 from msgplugins.msgcmd.cmdaz import CMD
+from qqsdk.message import MsgHandler, GroupMsg, FriendMsg
 
 
 class MenuPlugin(MsgHandler):
@@ -10,18 +9,21 @@ class MenuPlugin(MsgHandler):
     def collect_cmd(self, msg: GroupMsg | FriendMsg):
         friend_cmds: list[str] = []
         group_cmds: list[str] = []
+        last_name = ""
         for handler in msg.qq_client.msg_handlers:
             if not handler.desc:
                 continue
             handler: MsgHandler
             if not handler.check_enabled():
                 continue
+            desc_start_str = "\n" if handler.name == last_name else "\n\n"
+            last_name = handler.name
             if FriendMsg in handler.bind_msg_types:
-                friend_cmds.append(handler.desc)
+                friend_cmds.append(desc_start_str + handler.desc)
             if GroupMsg in handler.bind_msg_types and isinstance(msg, GroupMsg):
                 if not handler.check_enabled(msg.group.qq):
                     continue
-                group_cmds.append(handler.desc)
+                group_cmds.append(desc_start_str + handler.desc)
 
         if isinstance(msg, GroupMsg):
             return group_cmds
@@ -33,6 +35,8 @@ class MenuPlugin(MsgHandler):
         if CMD("菜单", alias=["功能", "帮助"]).az(msg.msg) or (not msg.msg.strip() and getattr(msg, "is_at_me", False)):
             if msg.quote_msg and not msg.msg.strip():
                 return
+            if isinstance(msg, GroupMsg) and msg.is_at_other:
+                return
             msg.destroy()
-            res = "命令列表(@机器人后发送命令即可触发)：\n\n" + "\n\n".join(self.collect_cmd(msg))
+            res = "命令列表(@机器人后发送命令即可触发)：" + "".join(self.collect_cmd(msg))
             msg.reply(res)
