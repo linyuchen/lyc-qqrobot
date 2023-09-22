1bbae7c51feaaa0283f8ecd8c1c13a82147f48e0
linyuchen <lin.yu.chen@foxmail.com>
2023-09-22 15:42:42 +0800

    fix: playwright thread safe

--- msgplugins/browser_preview/browser_screenshot.py
+++ msgplugins/browser_preview/browser_screenshot.py
@@ -3,37 +3,25 @@ from pathlib import Path
 from urllib.parse import quote
 from playwright.sync_api import sync_playwright
 
-p = None
-browser = None
-
-
-def get_browser():
-    global browser
-    if browser:
-        return browser
-    else:
-        global p
-        p = sync_playwright().start()
-        browser = p.chromium.launch()
-        return browser
-
 
 def search_baidu(keyword) -> Path:
     keyword = quote(keyword.encode("utf8"))
-    page = get_browser().new_page()
-    page.goto(f"https://www.baidu.com/s?wd={keyword}")
-    e = page.locator("css=#content_left")
-    e.evaluate("""
-    e = document.getElementById("content_left")
-    e.style.paddingLeft = "10px";
-    e.style.paddingRight = "10px";
-    document.getElementById("head").style.display="none";
-    try{
-        document.getElementById("searchTag").style.display="none";
-    }catch{}
-    """)
-    path = Path(tempfile.mktemp(suffix=".png"))
-    e.screenshot(path=path)
-    page.close()
-    return path
 
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(f"https://www.baidu.com/s?wd={keyword}")
+        e = page.locator("css=#content_left")
+        e.evaluate("""
+        e = document.getElementById("content_left")
+        e.style.paddingLeft = "10px";
+        e.style.paddingRight = "10px";
+        document.getElementById("head").style.display="none";
+        try{
+            document.getElementById("searchTag").style.display="none";
+        }catch{}
+        """)
+        path = Path(tempfile.mktemp(suffix=".png"))
+        e.screenshot(path=path)
+        page.close()
+        return path
