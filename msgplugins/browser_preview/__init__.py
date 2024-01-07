baa9f1ba686ba8285fd0a6a745562123a5012bea
linyuchen <lin.yu.chen@foxmail.com>
2024-01-07 18:17:12 +0800

    feature: 网页预览加入频率检测，避免重复刷屏

--- msgplugins/browser_preview/__init__.py
+++ msgplugins/browser_preview/__init__.py
@@ -1,11 +1,29 @@
 import re
+import time
 
 import config
 from msgplugins.msgcmd import on_command
-from qqsdk.message import GeneralMsg, MessageSegment
+from qqsdk.message import GeneralMsg, MessageSegment, FriendMsg
 from .browser_screenshot import search_baidu, ZhihuPreviewer, github_readme, wx_article, moe_wiki
 
 zhihu_previewer = ZhihuPreviewer()
+
+url_history = {}  # {"g群号": {"http://xxx": "最后一次获取时间戳"}}
+
+
+def check_url_recent(qq: str, url: str) -> bool:
+    now = time.time()
+    if qq not in url_history:
+        url_history[qq] = {}
+
+    if url not in url_history[qq]:
+        url_history[qq][url] = now
+        return False
+    if now - url_history[qq][url] > 5 * 60:
+        url_history[qq][url] = now
+        return False
+    else:
+        return True
 
 
 @on_command("百度",
@@ -25,9 +43,11 @@ def baidu(msg: GeneralMsg, params: list[str]):
 
 @on_command("", cmd_group_name="知乎预览",
             desc="知乎链接获取预览图",
-            example="https://www.zhihu.com/question/123456"
+            example="https://www.zhihu.com/question/123456",
+            auto_destroy=False
             )
 def zhihu_preview(msg: GeneralMsg, params: list[str]):
+    qq_id = msg.friend.qq if isinstance(msg, FriendMsg) else msg.group.qq
     if "//www.zhihu.com/question" in msg.msg:
 
         """
@@ -43,7 +63,8 @@ def zhihu_preview(msg: GeneralMsg, params: list[str]):
         answer_url = answer_url[0] if answer_url else None
         url = answer_url or question_url
         if url:
-            msg.destroy()
+            if check_url_recent(qq_id, url):
+                return
             img_path = zhihu_previewer.zhihu_question(url)
             if img_path:
                 msg.reply(MessageSegment.image_path(img_path) + MessageSegment.text(url), at=False, quote=False)
@@ -53,7 +74,8 @@ def zhihu_preview(msg: GeneralMsg, params: list[str]):
         zhuanlan_url = re.findall(r"https://zhuanlan.zhihu.com/p/\d+", msg.msg)
         zhuanlan_url = zhuanlan_url[0] if zhuanlan_url else None
         if zhuanlan_url:
-            msg.destroy()
+            if check_url_recent(qq_id, zhuanlan_url):
+                return
             img_path = zhihu_previewer.zhihu_zhuanlan(zhuanlan_url)
             if img_path:
                 msg.reply(MessageSegment.image_path(img_path) + MessageSegment.text(zhuanlan_url),
@@ -63,7 +85,8 @@ def zhihu_preview(msg: GeneralMsg, params: list[str]):
 
 @on_command("", cmd_group_name="github预览",
             desc="github链接获取README预览图",
-            example="https://github.com/linyuchen/qqrobot-plugin"
+            example="https://github.com/linyuchen/qqrobot-plugin",
+            auto_destroy=False,
             )
 def github_preview(msg: GeneralMsg, params: list[str]):
     if "//github.com/" in msg.msg:
@@ -71,7 +94,9 @@ def github_preview(msg: GeneralMsg, params: list[str]):
         url = re.findall(r"https://github.com/[a-zA-Z0-9_/-]+", msg.msg)
         url = url[0] if url else None
         if url:
-            msg.destroy()
+            qq_id = msg.friend.qq if isinstance(msg, FriendMsg) else msg.group.qq
+            if check_url_recent(qq_id, url):
+                return
             img_path = github_readme(url, http_proxy=config.get_config("GFW_PROXY"))
             if img_path:
                 msg.reply(MessageSegment.image_path(img_path) + MessageSegment.text(url), at=False, quote=False)
@@ -92,7 +117,8 @@ def moe_wiki_cmd(msg: GeneralMsg, params: list[str]):
 
 @on_command("", cmd_group_name="微信文章预览",
             desc="微信文章链接获取预览图",
-            example="https://mp.weixin.qq.com/s/xa33f"
+            example="https://mp.weixin.qq.com/s/xa33f",
+            auto_destroy=False
             )
 def wx_article_preview(msg: GeneralMsg, params: list[str]):
     if "mp.weixin.qq.com" in msg.msg:
@@ -100,7 +126,9 @@ def wx_article_preview(msg: GeneralMsg, params: list[str]):
         url = re.findall(r"https://mp.weixin.qq.com/s[/a-zA-Z0-9%?&=_-]+", msg.msg)
         url = url[0] if url else None
         if url:
-            msg.destroy()
+            qq_id = msg.friend.qq if isinstance(msg, FriendMsg) else msg.group.qq
+            if check_url_recent(qq_id, url):
+                return
             img_path = wx_article(url)
             if img_path:
                 msg.reply(MessageSegment.image_path(img_path) + MessageSegment.text(url), at=False, quote=False)
